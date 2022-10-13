ebb1aba6f7f77dd749746bc8e717fae304fda886
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2022-10-13 17:27:09 -0300

    Adicionando usuario arquivos

---
+++ prova/database/migrations/2022_10_13_202141_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome');
+            $table->string('cpf',11);
+            $table->string('sexo',1);
+            $table->string('endereco',100);
+            $table->string('cidade',40);
+            $table->string('estado',2);
+            $table->date('data_nascimento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
