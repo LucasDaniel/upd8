4ae1a6d82994b03f43cdf4ffa4bbfc2e1af92b52
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2022-10-13 23:54:45 -0300

    Adicionando seed

--- prova/database/seeders/UsuarioSeeder.php
+++ prova/database/seeders/UsuarioSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Usuario;
 
 class UsuarioSeeder extends Seeder
 {
@@ -14,6 +15,21 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //['nome','cpf','sexo','endereco','cidade','estado','data_nascimento'];
+        for ($i = 0; $i < 10; $i++) {
+            $usuario = new Usuario();
+            $usuario->nome = 'Nome teste '.$i;
+            $usuario->cpf = '1234543234'.$i;
+            if ($i < 3) $usuario->sexo = 'm';
+            else if ($i < 7) $usuario->sexo = 'f';
+            else $usuario->sexo = 'm';
+            $usuario->endereco = 'Rua tal '.$i;
+            if ($i < 5) $usuario->cidade = 'Rio de Janeiro';
+            else $usuario->cidade = 'São Paulo';
+            if ($i < 5) $usuario->estado = 'RJ';
+            else $usuario->estado = 'SP';
+            $usuario->data_nascimento = date('Y-m-'.($i+1));
+            $usuario->save();
+        }
     }
 }
